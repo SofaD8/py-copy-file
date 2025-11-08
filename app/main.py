cd4c693e493ff2b511412428b3a7b833d7bb5f04
Sofa <sofo4kavakula@gmail.com>
2025-11-08 21:48:36 +0200

    Rename source and taeget

--- app/main.py
+++ app/main.py
@@ -4,14 +4,17 @@ def copy_file(command: str) -> None:
     if len(parts) != 3 or parts[0] != "cp":
         return  # Невірна команда — нічого не робимо
 
-    source, target = parts[1], parts[2]
+    source_filename, destination_filename = parts[1], parts[2]
 
-    if source == target:
+    if source_filename == destination_filename:
         return  # Не копіюємо файл у самого себе
 
     try:
-        with (open(source, "r", encoding="utf-8")
-              as file_in, open(target, "w", encoding="utf-8") as file_out):
+        with (open(source_filename, "r", encoding="utf-8")
+              as file_in, open(
+            destination_filename,
+            "w", encoding="utf-8"
+        ) as file_out):
             file_out.write(file_in.read())
     except FileNotFoundError:
         pass
